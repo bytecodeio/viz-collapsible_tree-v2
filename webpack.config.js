feac665c9d06bc22e71a7e514c36768828a34484
TJ Banghart <tjbanghart@google.com>
2020-06-12 11:54:16 -0700

    Simple implementation - no complex drilling

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path')
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+var webpackConfig = {
+  mode: 'production',
+  entry: {
+    collapsible_tree: './src/collapsible_tree/collapsible_tree.ts',
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname),
+    library: "[name]",
+    libraryTarget: "umd"
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: [
+    new UglifyJSPlugin()
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, loader: "babel-loader" },
+      { test: /\.ts$/, loader: "ts-loader" },
+      { test: /\.css$/, loader: [ 'to-string-loader', 'css-loader' ] }
+    ]
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
+}
+
+module.exports = webpackConfig
